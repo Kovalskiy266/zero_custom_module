4bb2497934f4c1d1fd0ca76fda5f2cddd249a333
Koval <vitalkovalik@gmail.com>
2021-09-11 16:34:04 +0300

    Create a subtheme ans stylesheet

--- web/modules/custom/koval/src/Controller/CatsController.php
+++ web/modules/custom/koval/src/Controller/CatsController.php
@@ -9,9 +9,11 @@ class CatsController extends ControllerBase {
    * Builds the response.
    */
   public function build(): array {
+    $form = \Drupal::formBuilder()->getForm('Drupal\koval\Form\FormCats');
     $build['content'] = [
       '#theme' => 'koval-page',
       '#text' => $this->t('Hello! You can add here a photo of your cat.'),
+      '#form' => $form,
     ];
     return $build;
   }
